277d2aa4637584ef9c6121a4bfed5f6edb8dd0d0
Paul Smithdale <paul.smithdale@insight.com>
2020-08-11 09:09:36 +0800

    apps deploy

--- infrastructure/create.sh
+++ infrastructure/create.sh
@@ -1,1 +1,16 @@
 az group create -n $RG_NAME -l $RG_LOCATION
+
+az appservice plan create --name $APP_SERVICE_NAME \
+                          --resource-group $RG_NAME \
+                          --location $RG_LOCATION \
+                          --is-linux --sku "B1"
+
+az webapp create --name $WEB_BE_NAME \
+                 --plan $APP_SERVICE_NAME \
+                 --resource-group $RG_NAME \
+                 --runtime "DOTNETCORE|3.1"
+
+az webapp create --name $WEB_FE_NAME \
+                 --plan $APP_SERVICE_NAME \
+                 --resource-group $RG_NAME \
+                 --runtime $NODE_VERSION
